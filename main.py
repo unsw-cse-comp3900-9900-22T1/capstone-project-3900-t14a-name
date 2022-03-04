fa3869da1cf192521adb7797d7b29ea17d7e63c0
arzaanmirza <arzaanmirza@live.com>
2022-03-05 00:29:51 +1100

    adding registration functionality

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from tabnanny import check
 from flask import render_template, request, redirect, url_for, Flask, session
 from get_dynamodb import get_dynamodb
 from post_to_account_dynamodb import post_account_details
